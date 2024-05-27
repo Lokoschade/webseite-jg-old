7280c131bbd222b579b2825d84684fda2cd62426
DragonNextGen <jberyldinah@gmail.com>
2024-05-28 00:13:23 +0200

    Habe Text geändert, sowie Anpassungen der Anordnung vorgenommen. Anpassungen für die mobile Version. Die anderen Versionen mehrheitlich unverändert Habe es nicht hinbekommen nur h1 nowrap für 1050 Screen einzustellen.

--- src/components/vorstellung/Vorstellung.jsx
+++ src/components/vorstellung/Vorstellung.jsx
@@ -6,11 +6,11 @@ const Vorstellung = () => {
   return (
     <div className='jg__vorstellung' id='vorstellung'>
       <div className='jg__vorstellung-content'>
-        <h1 className='gradient__text'>We have a right to be informed</h1>
+        <h1 className='gradient__text'>Wir haben ein Recht auf Information</h1>
         <h2>Mitspracherecht durch Transparenz sichern</h2>
         <p> Ich bin Jamila Gränzer, Informatikstudentin des FB5 der <strong>Hochschule Anhalt</strong>.</p>
         <p>Die Hochschule und vor allem der Fachbereich 5 liegen mir sehr am Herzen und ich sehe viel Potential, das noch nicht genutzt wird.</p>
-        <p>Derzeit gibt es Prozesse in der Hochschule, die für Studierende nicht transparent sind, obwohl diese die Existenz von Studiengängen und Fachbereichen betreffen. Das finde ich inakzeptable.</p>
+        <p>Derzeit gibt es Prozesse in der Hochschule, die für Studierende nicht transparent sind, obwohl diese die Existenz von Studiengängen und Fachbereichen betreffen. Es ist Zeit, dass sich das ändert.</p>
       </div>
       <div className='jg__vorstellung-image'>
         <img src={jamila} alt="Jamila" />

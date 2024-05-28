54de34d4f910a301fa82446be6a502cf041ab118
Lokoschade <fr.ratteit@gmx.de>
2024-05-28 17:57:44 +0200

    Anfang von zweitem Abschnitt

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({title, text}) => {
   return (
-    <div>Feature</div>
+    <div className='jg__features-container__feature'>
+      <div className='jg__features-container__feature-text'>
+        {text}
+      </div>
+    </div>
   )
 }
 
--- src/components/problem/Problem.jsx
+++ src/components/problem/Problem.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import './problem.css'
-import Feature from '../feature/Feature'
 
 const Problem = () => {
   return (
-    <div className='jg__problem section__padding' id='problem'>
-      <div className='jg__problem-feature'>
-        <Feature />
-      </div>
+    <div className='jg__problem section__margin' id='problem'>
+      <div className='jg__problem-intro'>
+        <p>Wir, als Studierende, haben ein Recht auf Mitsprache, Information und Transparenz. Wir gestalten die Hochschule mit und lassen nicht zu, dass stillschweigend irreversible Veränderungen, die uns akut betreffen, ohne unser Einverständnis umgesetzt werden.</p>      </div>
       <div className='jg__problem-heading'>
-        <h1 className='gradient__text'>Ich setze mich für Transparenz, Information und Mitspracherecht für Studierende der HSA ein.</h1>
+        <h1 className='gradient__text'>Es ist nicht in Ordnung, dass</h1>
       </div>
       <div className='jg__problem-container'>
-        <Feature/>
-        <Feature/>
-        <Feature/>
+        <div className='jg__problem-container__item'>
+          <div/>
+          <p>aufgrund von Verschwiegenheitspflicht, mangelndem Interesse, mangelnder Information oder Fahrlässigkeit, die Studierendenschaft nicht über Prozesse, die ihren Fachbereich akut betreffen, informiert werden.</p>
+        </div>
+        <div className='jg__problem-container__item'>
+          <div/>
+          <p>hinter geschlossenen Türen über Entscheidungen diskutiert wird, die uns, Studierende, etwas angehen.</p>
+        </div>
+        <div className='jg__problem-container__item'>
+          <div/>
+          <p>es keine allgemeine Einsicht für Studierende in die Prozesse des StuRa und Senates gibt.</p>
+        </div>
       </div>
     </div>
   )
